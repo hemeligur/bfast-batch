9e93d61f09b58998cab803878931f01b87c45ab6
Guilherme F. A. Souza <guifas3000@gmail.com>
2017-06-06 14:31:28 -0300

    debug[8]

--- lib/utils.lib.r
+++ lib/utils.lib.r
@@ -315,11 +315,11 @@ cellFromPointOrPolygon = function(r, p, type){
 			invisible(beSureToLoad(c("rgeos", "raster")))
 
 			centr = gCentroid(p, byid = TRUE)
-			print(paste(p$ID, extent(centr)[2:3], collapse = ""))
+			print(paste(p$ID, as.character(extent(centr)[2:3]), collapse = ""))
 			centr_cell = cellFromXY(r, centr)
-			print(paste(p$ID, extent(centr)[2:3], centr_cell, collapse = ""))
+			print(paste(p$ID, as.character(extent(centr)[2:3]), centr_cell, collapse = ""))
 			pol_cells = cellFromPolygon(r, p, weights=F)[[1]]
-			print(paste(p$ID, extent(centr)[2:3], centr_cell, length(pol_cells), collapse = ""))
+			print(paste(p$ID, as.character(extent(centr)[2:3]), centr_cell, length(pol_cells), collapse = ""))
 			if(is.null(pol_cells)){
 				cells = centroid = NA
 			}
