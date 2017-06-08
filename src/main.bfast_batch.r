8c6fadd77354dd94f3ee7bde04ae1d40e542d446
Guilherme F. A. Souza <guifas3000@gmail.com>
2017-06-08 15:42:37 -0300

    looks like lines 90 to 92 have been swallowed by the devil

--- src/main.bfast_batch.r
+++ src/main.bfast_batch.r
@@ -88,6 +88,8 @@
 	print("Calculating nonna, startRow, startCol, h and dates")
 	nonna_result = nonna_mask(timeChange=timeChange, timeUnits=timeUnits, maskStr=maskStr, dataRasterStr=dataRasterStr)
 	
+
+
 	dataRasterStr = nonna_result$dataRasterTmpStr
 	maskStr = nonna_result$maskStr
 	print(paste("after nonna, maskStr:", maskStr, ".And nonna_result$maskStr", nonna_result$maskStr))
